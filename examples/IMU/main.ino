f29ccc77bea8256afb19f29428ccebe917db95a2
KGB-8375 <27stopittj@gmail.com>
2021-12-12 12:32:41 -0500

    start creating examples

---
+++ examples/IMU/main.ino
@@ -0,0 +1,13 @@
+/* Main should be mostly empty, since it only controls the main program logic */
+
+#include"imu.h" // the submodule code should be here
+
+void setup()
+{
+	
+}
+
+void loop()
+{
+	
+}
